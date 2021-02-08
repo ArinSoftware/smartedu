d52cbefd96a4b557608010f1a96b2b6b6ca90baf
Arin <arinyazilim@gmail.com>
2021-02-08 18:39:56 +0300

    Lesson 07

--- smartedu_con/smartedu/settings.py
+++ smartedu_con/smartedu/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+""" AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-]
+] """
 
 
 # Internationalization
