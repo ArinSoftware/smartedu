bac86c1e1f728a141b515489398e4453e9979ca5
Arin <arinyazilim@gmail.com>
2021-02-11 14:06:40 +0300

    Lesson 09

--- smartedu_con/courses/admin.py
+++ smartedu_con/courses/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from . models import Course
 
-admin.site.register(Course)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'available')
+    list_filter = ('available',)
+    search_fields = ('name', 'description')
