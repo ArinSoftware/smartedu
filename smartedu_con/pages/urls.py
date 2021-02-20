b1f215d4f1f46a6f00868c2b3b5d490551e03364
Arin <arinyazilim@gmail.com>
2021-02-20 03:40:38 +0300

    Lesson 13

--- smartedu_con/pages/urls.py
+++ smartedu_con/pages/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from pages.views import AboutView, IndexView
 
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('about/', views.about, name="about"),
+    path('', IndexView.as_view(), name="index"),
+    path('about/', AboutView.as_view(), name="about"),
     #path(route, view, opt(kısayol ismi))
 
 ]
--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from courses.models import Course
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(TemplateView):
+    template_name = 'index.html'
 
-def about(request):
-    return render(request, 'about.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['courses'] = Course.objects.filter(available=True).order_by('-date')[:2]
+        context['total_course'] = Course.objects.filter(available=True).count()
+        return context
+
+#def index(request):
+#    return render(request, 'index.html')
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+#def about(request):
+#   return render(request, 'about.html')
