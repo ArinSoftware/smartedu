cd9fc2af7e261a654e5111fae0ac73c2d837be65
Arin <arinyazilim@gmail.com>
2021-02-27 03:07:05 +0300

    Lesson 15

--- smartedu_con/teachers/urls.py
+++ smartedu_con/teachers/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from teachers.views import TeacherListView
+from teachers.views import TeacherListView, TeacherDetailView
 
 
 urlpatterns = [
     path('', TeacherListView.as_view(), name="teachers"),
-
+    path('teacher/<int:pk>', TeacherDetailView.as_view(), name="teacher_detail"),
 ]
--- smartedu_con/teachers/views.py
+++ smartedu_con/teachers/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from teachers.models import Teacher
+from courses.models import Course
 
 class TeacherListView(ListView):
     model = Teacher
     template_name = 'teachers.html'
     context_object_name = 'teachers'
-    #paginate_by = 1
+    #git pusjpaginate_by = 1
     #queryset = Teacher.objects.all()[:1]
 
     #def get_queryset(self):
         #return Teacher.objects.all()[:2]
+
+class TeacherDetailView(DetailView):
+    model = Teacher
+    template_name = 'teacher.html'
+    context_object_name = 'teacher'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['courses'] = Course.objects.filter(available=True, teacher = self.kwargs['pk'])
+        return context
