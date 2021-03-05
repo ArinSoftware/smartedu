1c5c836741e0567337f217d10dc056d80605e720
Arin <arinyazilim@gmail.com>
2021-03-06 00:56:55 +0300

    Lesson 19

--- smartedu_con/accounts/forms.py
+++ smartedu_con/accounts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from django.contrib.auth.forms UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={
--- smartedu_con/accounts/views.py
+++ smartedu_con/accounts/views.py
@@ -1,15 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 
 def user_login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username,
+                                        password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('index')
+
+                else:
+                    messages.info(request, 'Disabled Account')
+
+            else:
+                messages.info(request, 'Check Your Username and Password')
+
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form':form})
+
 
 def user_register(request):
-    return render(request, 'register.html')
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account has been created, You can LOGIN')
+            return redirect('login')
+    
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html', {'form':form})
+
 
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect('index')
 
 def user_dashboard(request):
     pass
